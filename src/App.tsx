441690daafdd2fd7372b6fabb16359c0961c5916
mvoloskov <hello@miloslav.website>
2021-06-07 18:28:25 +0300

    basic funtionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import sizes from './sizes.json'
+import { FileInfo, Widget as UploadcareUpload } from '@uploadcare/react-widget'
+import Size from './components/size'
 
 const App: React.FC = () => {
-	console.log(sizes)
+
+	const [src, setSrc] = React.useState<string | null>(null)
+
+	const sizesForSimpleMode = sizes.filter(size => size.simple)
+	const uploadOnChange = React.useCallback((fileInfo: FileInfo) => {
+		setSrc(fileInfo.cdnUrl)
+	}, [])
+
 	return (
 		<div>
-			Hello world!
+			<UploadcareUpload
+				publicKey='3828f4d78b7de9c98461'
+				onChange={uploadOnChange}
+			/>
+
+			{src !== null && sizesForSimpleMode.map(size => (
+				<Size
+					key={`${size.app}_${size.name}_${size.width}x${size.height}`}
+					src={src}
+					app={size.app}
+					name={size.name}
+					width={size.width}
+					height={size.height}
+				/>
+			))}
 		</div>
 	)
 }
---
+++ src/components/size.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import css from './size.module.css'
+
+const ensureEndingWithSlash = (str: string) => {
+	const lastSymbol = str[str.length - 1]
+	if (lastSymbol === '/') return str
+	return `${str}/`
+}
+
+interface Props {
+	src: string,
+	app: string,
+	name: string,
+	width: number,
+	height: number,
+}
+
+const Size: React.FC<Props> = props => {
+	const srcWithSlash = ensureEndingWithSlash(props.src)
+	const thisSizeSrc = `${srcWithSlash}-/scale_crop/${props.width}x${props.height}/smart/-/quality/smart/`
+
+	return (
+		<div>
+			<h2>
+				{props.app} — {props.name}
+			</h2>
+         <p>
+            {props.width}x{props.height}
+         </p>
+			<img
+            className={css.sizeImg}
+				src={thisSizeSrc}
+				width={props.width}
+				height={props.height}
+				alt={`${props.name} for ${props.app} (${props.width}x${props.height})`}
+			/>
+		</div>
+	)
+}
+
+export default Size

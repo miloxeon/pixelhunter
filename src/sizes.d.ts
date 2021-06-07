f4db11686a9de0f58e930e0cae0b654e06bf6f74
mvoloskov <hello@miloslav.website>
2021-06-07 17:23:02 +0300

    basic sizes added

---
+++ src/sizes.d.ts
@@ -0,0 +1,30 @@
+interface TargetApp {
+	/**
+	 * @description  taget service: Facebook, Instagram, etc. Case-sensitive.
+	 */
+	app: string,
+
+	/**
+	 * @description  taget service: short description of this size: Avatar, Cover, etc. Should be unique.
+	 */
+	name: string,
+
+	/**
+	 * @description  width in pixels
+	 */
+	width: number,
+
+	/**
+	 * @description height in pixels
+	 */
+	height: number,
+
+	/**
+	 * @description should this target size be featured in "Simple mode"
+	 */
+	simple?: boolean,
+}
+
+type Sizes = array<TargetApp>
+
+export default Sizes

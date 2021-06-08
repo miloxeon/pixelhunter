dffbc1339bc3acda45df0d20751b6ad33f6a67b1
mvoloskov <hello@miloslav.website>
2021-06-09 02:14:04 +0300

    trying to optimizesafari performance

--- src/components/size/index.tsx
+++ src/components/size/index.tsx
@@ -13,42 +13,41 @@ interface Props extends SizeWithSrc {
 }
 
 const zoomDuration = 300
+const tiltSpeed = zoomDuration
 
 const Size: React.FC<Props> = props => {
 	const url = getUrl(props.src, props.width, props.height, props.compress)
 	const alt = getSizeKey(props)
-	const imageWrapperRef = React.useRef<HTMLDivElement & HTMLVanillaTiltElement>(null)
+	const id = `${props.app}${props.name}${props.width}x${props.height}`.replaceAll(' ', '')
+	const tiltWrapperRef = React.useRef<HTMLDivElement & HTMLVanillaTiltElement>(null)
 
 	const [isZoomed, setIsZoomed] = React.useState<boolean>(false)
 
 	const handleZoomChange = React.useCallback(shouldZoom => {
-		if (shouldZoom) {
-			imageWrapperRef.current?.vanillaTilt?.destroy()
-		}
+		// if (shouldZoom) tiltWrapperRef.current?.vanillaTilt?.destroy()
 		setIsZoomed(shouldZoom)
 	}, [])
 
-	React.useEffect(() => {
-		const current = imageWrapperRef.current
-		if (!current) return
+	// React.useEffect(() => {
+	// 	const current = tiltWrapperRef.current
+	// 	if (!current) return
 
-		if (isZoomed && current.vanillaTilt) {
-			current.vanillaTilt.destroy()
-		}
+	// 	if (!isZoomed && !current.vanillaTilt) {
+	// 		setTimeout(() => Tilt.init(current, {
+	// 			max: 3,
+	// 			perspective: 2000,
+	// 			scale: 1.02,
+	// 			easing: 'cubic-bezier(.17, .67, .24, 1.01)',
+	// 			glare: true,
+	// 			'max-glare': 0.5,
+	// 			'mouse-event-element': `#${id}`,
+	// 			speed: tiltSpeed,
+	// 			transition: false,
+	// 		}), zoomDuration)
+	// 	}
 
-		if (!isZoomed && !current.vanillaTilt) {
-			setTimeout(() => Tilt.init(current, {
-				max: 3,
-				perspective: 2000,
-				scale: 1.02,
-				easing: 'cubic-bezier(.17, .67, .24, 1.01)',
-				glare: true,
-				'max-glare': 0.5,
-			}), zoomDuration)
-		}
-
-		return () => current?.vanillaTilt?.destroy()
-	}, [isZoomed])
+	// 	return () => current?.vanillaTilt?.destroy()
+	// }, [isZoomed, id])
 
 	return (
 		<div className={css.root}>
@@ -56,41 +55,59 @@ const Size: React.FC<Props> = props => {
 				{props.name}
 			</h3>
 
-			<div className={css.imageWrapper} ref={imageWrapperRef}>
+			<div className={css.imageWrapper} id={id}>
 				<ControlledZoom
 					isZoomed={isZoomed}
 					onZoomChange={handleZoomChange}
 					transitionDuration={zoomDuration}
 					overlayBgColorEnd='var(--b)'
 					zoomMargin={30}
+					wrapStyle={{
+						position: 'absolute',
+						top: 0,
+						left: 0,
+						right: 0,
+						bottom: 0,
+						zIndex: 3,
+					}}
 				>
 					<img
-						className={!isZoomed ? css.image : css.imageZoomed}
+						className={!isZoomed ? css.fakeImage : css.fakeImageZoomed}
+						src={url}
+						width={props.width}
+						height={props.height}
+						alt=''
+						aria-hidden={true}
+					/>
+				</ControlledZoom>
+				<div className={css.imgTiltWrapper} ref={tiltWrapperRef}>
+					<img
+						className={css.image}
 						src={url}
 						width={props.width}
 						height={props.height}
 						alt={alt}
 					/>
-				</ControlledZoom>
-				<div className={css.infoOverlay}>
-					<div className={css.infoOverlayContent}>
-						<p className={css.infoOverlayText}>
-							PNG, {props.width}x{props.height}
-						</p>
-						<a
-							className={css.download}
-							href={url}
-							target='_blank'
-							rel='noreferrer'
-							download={`${alt}.png`}
-							aria-label={`Download ${alt}.png`}
-							title={`Download ${alt}.png`}
-						>
-							<FiArrowDown aria-hidden={true} />
-						</a>
+					<div className={css.infoOverlay}>
+						<div className={css.infoOverlayContent}>
+							<p className={css.infoOverlayText}>
+								PNG, {props.width}x{props.height}
+							</p>
+							<a
+								className={css.download}
+								href={url}
+								target='_blank'
+								rel='noreferrer'
+								download={`${alt}.png`}
+								aria-label={`Download ${alt}.png`}
+								title={`Download ${alt}.png`}
+							>
+								<FiArrowDown aria-hidden={true} />
+							</a>
+						</div>
 					</div>
+					<div className={css.gradient} aria-hidden={true} />
 				</div>
-				<div className={css.gradient} aria-hidden={true} />
 			</div>
 		</div>
 	)

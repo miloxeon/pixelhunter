c91e8e72dc2de5beb9a26e8f7bc553c19eba5ae0
mvoloskov <hello@miloslav.website>
2021-06-08 15:06:12 +0300

    beautiful Size component

--- src/components/size/index.tsx
+++ src/components/size/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { FiArrowDown } from 'react-icons/fi'
 import css from './size.module.css'
 import { SizeWithSrc } from '../../types'
-import { getUrl } from '../../helpers'
+import { getUrl, getSizeKey } from '../../helpers'
 
 interface Props extends SizeWithSrc {
 	compress: boolean
@@ -9,22 +10,42 @@ interface Props extends SizeWithSrc {
 
 const Size: React.FC<Props> = props => {
 	const url = getUrl(props.src, props.width, props.height, props.compress)
+	const alt = getSizeKey(props)
 
 	return (
 		<div>
 			<h2>
 				{props.app} — {props.name}
 			</h2>
-			<p>
-				{props.width}x{props.height}
-			</p>
-			<img
-				className={css.sizeImg}
-				src={url}
-				width={props.width}
-				height={props.height}
-				alt={`${props.name} for ${props.app} (${props.width}x${props.height})`}
-			/>
+
+			<div className={css.imageWrapper}>
+				<img
+					className={css.image}
+					src={url}
+					width={props.width}
+					height={props.height}
+					alt={alt}
+				/>
+				<div className={css.infoOverlay}>
+					<div className={css.infoOverlayContent}>
+						<p className={css.infoOverlayText}>
+							PNG, {props.width}x{props.height}
+						</p>
+						<a
+							className={css.download}
+							href={url}
+							target='_blank'
+							rel='noreferrer'
+							download={`${alt}.png`}
+							aria-label={`Download ${alt}.png`}
+							title={`Download ${alt}.png`}
+						>
+							<FiArrowDown aria-hidden={true} />
+						</a>
+					</div>
+				</div>
+				<div className={css.gradient} aria-hidden={true} />
+			</div>
 		</div>
 	)
 }

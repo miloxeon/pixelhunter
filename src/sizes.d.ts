f99dd114eeb3ebd851826d676b21ef44cbb848ac
mvoloskov <hello@miloslav.website>
2021-06-08 15:36:56 +0300

    schema changed to accomodate SEO description

--- src/sizes.d.ts
+++ src/sizes.d.ts
@@ -1,9 +1,4 @@
-interface TargetApp {
-	/**
-	 * @description  taget service: Facebook, Instagram, etc. Case-sensitive.
-	 */
-	app: string,
-
+interface Size {
 	/**
 	 * @description  taget service: short description of this size: Avatar, Cover, etc. Should be unique.
 	 */
@@ -23,6 +18,25 @@ interface TargetApp {
 	 * @description should this target size be featured in "Simple mode"
 	 */
 	simple?: boolean,
+}
+
+interface TargetApp {
+	/**
+	 * @description  taget service: Facebook, Instagram, etc. Case-sensitive.
+	 */
+	app: string,
+
+	/**
+	 * @description  some SEO description text about the target service.
+	 */
+	description: string,
+
+	/**
+	 * @description  a logo image src. Will be put in an img tag and inserted as-is.
+	 */
+	logoSrc: string,
+
+	sizes: Size[],
 }
 
 type Sizes = TargetApp[]

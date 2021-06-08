a96e4d6817237dd0a1be4f648ce0dfb868755f53
mvoloskov <hello@miloslav.website>
2021-06-09 00:40:02 +0300

    image-zoom added

--- src/components/size/index.tsx
+++ src/components/size/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Tilt, { HTMLVanillaTiltElement } from 'vanilla-tilt'
+import { Controlled as ControlledZoom } from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 import { FiArrowDown } from 'react-icons/fi'
 import css from './size.module.css'
 import { SizeWithSrc } from '../../types'
@@ -10,25 +12,50 @@ interface Props extends SizeWithSrc {
 	app: string,
 }
 
+const zoomDuration = 300
+
 const Size: React.FC<Props> = props => {
 	const url = getUrl(props.src, props.width, props.height, props.compress)
 	const alt = getSizeKey(props)
 	const imageWrapperRef = React.useRef<HTMLDivElement & HTMLVanillaTiltElement>(null)
 
+	const [isZoomed, setIsZoomed] = React.useState<boolean>(false)
+
+	const handleZoomChange = React.useCallback(shouldZoom => {
+		if (shouldZoom) {
+			imageWrapperRef.current?.vanillaTilt?.destroy()
+		}
+		setIsZoomed(shouldZoom)
+	}, [])
+
 	React.useEffect(() => {
 		const current = imageWrapperRef.current
 		if (!current) return
 
-		Tilt.init(current, {
-			max: 3,
-			perspective: 2000,
-			scale: 1.02,
-			easing: 'cubic-bezier(.17, .67, .24, 1.01)',
-			glare: true,
-			'max-glare': 0.5,
-		})
+		if (isZoomed && current.vanillaTilt) {
+			current.vanillaTilt.destroy()
+		}
 
-		return () => current && current.vanillaTilt?.destroy()
+		if (!isZoomed && !current.vanillaTilt) {
+			setTimeout(() => Tilt.init(current, {
+				max: 3,
+				perspective: 2000,
+				scale: 1.02,
+				easing: 'cubic-bezier(.17, .67, .24, 1.01)',
+				glare: true,
+				'max-glare': 0.5,
+			}), zoomDuration)
+		}
+
+		return () => current?.vanillaTilt?.destroy()
+	}, [isZoomed])
+
+	React.useEffect(() => {
+		document.querySelectorAll('div[data-rmiz-wrap] button[type="button"]').forEach(button => {
+			button.removeAttribute('aria-label')
+			button.setAttribute('z-index', '-1')
+			button.setAttribute('aria-hidden', 'true')
+		})
 	}, [])
 
 	return (
@@ -38,13 +65,21 @@ const Size: React.FC<Props> = props => {
 			</h3>
 
 			<div className={css.imageWrapper} ref={imageWrapperRef}>
-				<img
-					className={css.image}
-					src={url}
-					width={props.width}
-					height={props.height}
-					alt={alt}
-				/>
+				<ControlledZoom
+					isZoomed={isZoomed}
+					onZoomChange={handleZoomChange}
+					transitionDuration={zoomDuration}
+					overlayBgColorEnd='var(--b)'
+					zoomMargin={30}
+				>
+					<img
+						className={isZoomed ? css.imageZoomed : css.image}
+						src={url}
+						width={props.width}
+						height={props.height}
+						alt={alt}
+					/>
+				</ControlledZoom>
 				<div className={css.infoOverlay}>
 					<div className={css.infoOverlayContent}>
 						<p className={css.infoOverlayText}>

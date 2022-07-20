31edf4a9db47891813d1e44ee2116dcbf18b0452
mvoloskov <hello@miloslav.website>
2022-07-20 19:37:53 +0400

    works again

--- src/App.tsx
+++ src/App.tsx
@@ -99,7 +99,6 @@ const App: React.FC<Props> = props => {
 
 	return (
 		<>
-			<div className={css.dsaoifasnflk}>The project is under maintenance.</div>
 			<Container>
 				<div className={css.hero} id='hero'>
 					<div className={css.content}>
